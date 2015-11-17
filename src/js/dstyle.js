0a9d9e6796fa52d1ccad689b93eb111f07c6d96a
dstyle0210 <dstyle0210@gmail.com>
2015-11-17 15:01:15 +0900

    티스토리 디자인 일부 변경.

--- src/js/dstyle.js
+++ src/js/dstyle.js
@@ -2,7 +2,7 @@ require.config({
     baseUrl : "js",
     paths : {
         "jquery":"https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min",
-        "react":"https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/react",
+        "react":"https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/react.min",
         "bootstrap":"https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min",
         "underscore":"https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min",
         "easypiechart":"https://cdnjs.cloudflare.com/ajax/libs/easy-pie-chart/2.1.4/jquery.easypiechart.min",
